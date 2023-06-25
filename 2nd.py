d4343da54c7e1db923c03382397b611b320210bc
MD RIJWAN <125175566+Rijwan1267@users.noreply.github.com>
2023-06-25 20:41:41 +0530

    First Unique Character in a String
    Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
    
    Example 1:
    Input: s = "leetcode"
    Output: 0
    
    Example 2:
    Input: s = "loveleetcode"
    Output: 2
    
    Example 3:
    Input: s = "aabb"
    Output: -1
    
    Constraints:
    a. 1 <= s.length <= 10^5
    b. s consists of only lowercase English letters.

---
+++ 2nd.py
@@ -0,0 +1,24 @@
+def firstUniqChar(s):
+    """
+    Finds the index of the first non-repeating character in the given string.
+    """
+    char_freq = {}
+    
+    for char in s:
+        char_freq[char] = char_freq.get(char, 0) + 1
+    
+    for i in range(len(s)):
+        if char_freq[s[i]] == 1:
+            return i
+    
+    return -1
+
+
+s1 = "leetcode"
+print(firstUniqChar(s1))  
+
+s2 = "loveleetcode"
+print(firstUniqChar(s2))  
+
+s3 = "aabb"
+print(firstUniqChar(s3)) 
